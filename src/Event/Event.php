5dce47d081a74844c2e72cd82ca78e193e4bc67e
janhuang <384099566@qq.com>
2016-07-13 10:09:03 +0800

    init

---
+++ src/Event/Event.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ *
+ * @author    jan huang <bboyjanhuang@gmail.com>
+ * @copyright 2016
+ *
+ * @link      https://www.github.com/janhuang
+ * @link      http://www.fast-d.cn/
+ */
+
+namespace FastD\Event;
+
+class Event
+{
+    public function on($name, $callback)
+    {
+
+    }
+
+    public function trigger($name)
+    {
+        
+    }
+}
